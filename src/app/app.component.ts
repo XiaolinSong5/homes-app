7ca1d5aea3a8821892579dde850f6805d07d3fa4
Xiaolin <xiaolin.song@benerail.com>
2024-11-16 14:58:06 +0100

    incorporate HomeComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HomeComponent} from "./home/home.component";
 
 @Component({
   standalone: true,
@@ -10,8 +11,13 @@ src="/assets/logo.svg"
 alt="logo"
 aria-hidden="true">
 </header>
+<section class="content">
+<app-home></app-home>
+
+</section>
 </main>`,
   styleUrls: ['./app.component.css'],
+  imports: [HomeComponent]
 })
 export class AppComponent {
   title = 'homes';
