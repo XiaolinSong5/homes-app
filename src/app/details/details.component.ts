2f9be0d28ae4dcde71a22cfff67bdef0d85513a0
Xiaolin <xiaolin.song@benerail.com>
2024-11-17 20:16:04 +0100

    Form added

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import {ActivatedRoute} from "@angular/router";
 import {HousingService} from "../housing.service";
 import {HousingLocation} from "../housing-location";
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
 <article>
 <img class="listing-photo" [src]=housingLocation?.photo>
@@ -32,7 +33,19 @@ Does this location has laundry: {{housingLocation?.laundry}}
 </section>
 <section>
 <h2 class="section-heading">Apply to live here</h2>
-<button class="primary" type="button">Apply now</button>
+<form [formGroup]="applyForm">
+<label for="first-name">First Name</label>
+<input id="first-name" type="text" formControlName="firstName">
+
+<label id="last-name">Last Name</label>
+<input id="last-name" type="text" formControlName="lastName">
+
+<label for="email">Email</label>
+<input id="email" type="text" formControlName="email">
+
+<button class="primary" type="submit">Apply now</button>
+
+</form>
 </section>
 </article>
   `,
@@ -42,6 +55,11 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService= inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(""),
+    lastName: new FormControl(""),
+    email: new FormControl("")
+  });
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
